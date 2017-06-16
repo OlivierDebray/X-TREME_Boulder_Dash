c70d06f108a901bdcaded8376e8f4583748f89cb
OlivierDebray <olivier.debray@viacesi.fr>
2017-06-16 10:38:01 +0200

    Code Bouchonné Main Final
    Code bouchonné complet pour le package main

---
+++ main/src/main/java/main/GameFrame.java
@@ -0,0 +1,38 @@
+package main;
+
+import contract.IEventPerformer;
+import contract.IGraphicsBuilder;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Observable;
+
+/**
+ * @author Olivier Debray olivier.debray@viacesi.fr
+ *         Made on 16/06/2017
+ */
+
+public class GameFrame extends JFrame implements KeyListener {
+    private static long serialVersionUID = -1112124206501543946L ;
+    public IEventPerformer eventPerformer ;
+
+    public void GameFrame (String title , IEventPerformer eventPerformer , IGraphicsBuilder graphicsBuilder , Observable observable) throws HeadLessException {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
---
+++ main/src/main/java/main/XTremeBoulderDash.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * @author Olivier Debray olivier.debray@viacesi.fr
+ *         Made on 16/06/2017
+ */
+
+public class XTremeBoulderDash {
+    public static void main (String[] args) {
+
+    }
+}
