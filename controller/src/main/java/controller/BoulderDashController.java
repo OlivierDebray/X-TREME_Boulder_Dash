e19a32acdce5e95246a4da652212c263f1abf297
Vincent-Alayrac <vincent.alayrac@viacesi.fr>
2017-06-16 10:57:53 +0200

    Code Bouchonné Controller Final
    Code bouchonné final du package controller

---
+++ controller/src/main/java/controller/BoulderDashController.java
@@ -0,0 +1,29 @@
+package controller;
+
+/**
+ * Created by Vincent on 16/06/2017.
+ */
+public class BoulderDashController {
+
+    private int TIME_SLEEP = 30;
+
+    private IBoulderDashModel boulderDashModel;
+
+    private boolean isGameOver = false;
+
+    public void BoulderDashController (IBoulderDashModel boulderDashModel){
+    }
+
+    public void orderPerform (IUserOrder userOder){
+    }
+
+    public void play(){
+    }
+
+    public void gameLoop(){
+    }
+
+    public void setViewSystem (IViewSystem viewSystem){
+    }
+
+}
