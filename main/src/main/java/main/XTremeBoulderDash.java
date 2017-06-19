3afcbb41af6c6b29e993a8f754858104db53244a
OlivierDebray <olivier.debray@viacesi.fr>
2017-06-19 15:01:15 +0200

    V1 main
    Instanciation des différents composants

--- main/src/main/java/main/XTremeBoulderDash.java
+++ main/src/main/java/main/XTremeBoulderDash.java
@@ -1,5 +1,9 @@
 package main;
 
+import controller.BoulderDashController;
+import model.BoulderDashModel;
+import view.BoulderDashView;
+
 /**
  * @author Olivier Debray olivier.debray@viacesi.fr
  *         Made on 16/06/2017
@@ -7,6 +11,15 @@ package main;
 
 public class XTremeBoulderDash {
     public static void main (String[] args) {
+        final BoulderDashModel boulderDashModel = new BoulderDashModel();
 
+        /*
+        Créer ici de quoi instancier tous les éléments visibles du jeu (héros, ennemis, map, ...)
+         */
+
+        final BoulderDashController boulderDashController = new BoulderDashController(boulderDashModel);
+        final BoulderDashView dogfightView = new BoulderDashView(boulderDashController, boulderDashModel, boulderDashModel);
+        boulderDashController.setViewSystem(dogfightView);
+        boulderDashController.play();
     }
 }
