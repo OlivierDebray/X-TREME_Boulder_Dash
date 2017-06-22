d2bce94d57f417e63f44046832bc23803d4bb49d
TheoRobineau <theo.robineau@viacesi.fr>
2017-06-22 10:20:50 +0200

    Correction de QueryLevel

--- model/src/main/java/model/dao/QueryLevel.java
+++ model/src/main/java/model/dao/QueryLevel.java
@@ -3,7 +3,7 @@ package model.dao;
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import model.Map;
+
 
 /**
  * Created by Théo on 21/06/2017.
@@ -19,7 +19,6 @@ public abstract class QueryLevel extends AbstractDAO {
 
     public String getQuerySelectByIdMap(int idmap) throws SQLException {
         CallableStatement callStatement = prepareCall(sqlMapByID);
-        Level level = null;
         callStatement.setInt(1, idmap);
         if (callStatement.execute()) {
             ResultSet result = callStatement.getResultSet();
